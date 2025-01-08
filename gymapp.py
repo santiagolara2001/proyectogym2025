cc75f03e5616d6e838117872f130acf31eb161c7
Santiago <santiagolsabas@gmail.com>
2025-01-08 11:54:57 -0600

    'prueba 8/1/2025'

--- gymapp.py
+++ gymapp.py
@@ -1,2 +1,9 @@
-import streamlit
-import pandas
+import streamlit as st
+
+# Title of the app
+st.title("Hello, Streamlit!")
+
+# Simple message
+st.write("This is a simple Streamlit app to test the commit.")
+
+# Run the app with `streamlit run app.py` if you save this as `app.py`
